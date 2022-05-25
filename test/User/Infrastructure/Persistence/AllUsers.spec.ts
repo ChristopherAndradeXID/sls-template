a071f9337aaafef781c74f0fcaa1ce7d085da77d
Christopher Gerardy Andrade Lazcano <candrade-xid@MacBook-Air-de-Christopher.local>
2022-05-24 21:42:18 -0500

    test on pull request

--- test/User/Infrastructure/Persistence/AllUsers.spec.ts
+++ test/User/Infrastructure/Persistence/AllUsers.spec.ts
@@ -1,12 +1,13 @@
-import {AllUserStub} from "../../mocks/AllUserStub";
+import {AllUserStub} from "../../Mocks/AllUserStub";
 import {UserIdMother} from "../../Domain/UserIdMother";
 
-const allUserStub = new AllUserStub();
-
 describe('AllUsers repository should', () => {
-    test('Get and user with uuid: c5c9da35-a45c-4c5f-9293-510b0c48dbdb', async () => {
-        const user = await allUserStub.withId(UserIdMother.create('c5c9da35-a45c-4c5f-9293-510b0c48dbdb'));
-        expect(user).not.toBeUndefined();
-        expect(user.id.value).toEqual('c5c9da35-a45c-4c5f-9293-510b0c48dbdb');
+    it('Find a user with uuid: c5c9da35-a45c-4c5f-9293-510b0c48dbdb', async () => {
+        const allUserStub = new AllUserStub();
+        const givenUserId = UserIdMother.create('c5c9da35-a45c-4c5f-9293-510b0c48dbdb');
+
+        const userResearched = await allUserStub.withId(givenUserId);
+
+        expect(userResearched.id.value).toEqual('c5c9da35-a45c-4c5f-9293-510b0c48dbdb');
     });
 });
